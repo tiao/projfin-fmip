bd70ca1159ac2faa7417afe0fe2753b7e47882fb
f.almeida87 <f.almeida87@f8e4e3fe-a248-0410-bbe3-834c725c7bcb>
2008-04-28 14:53:53 +0000

    Subcommitted by Filipe

--- uml_switch/branches/port.h
+++ uml_switch/branches/port.h
@@ -11,9 +11,8 @@
 extern int handle_port(int fd);
 extern void close_port(int fd);
 extern int setup_sock_port(int fd, struct sockaddr_un *name, int data_fd);
-extern int setup_port(int fd, void (*sender)(int fd, void *packet, int len, 
-					     void *data), void *data, 
-		      int data_len);
+extern int setup_port(int fd, void (*sender)(int fd, void *packet, int len,
+		void *data), void *data, int data_len);
 extern void handle_tap_data(int fd, int hub);
 extern void handle_sock_data(int fd, int hub);
 
